eb45ac3f2d92517fa388a1be216e7fc3a95725ff
qinxiao-1 <56571188+qinxiao-1@users.noreply.github.com>
2024-02-07 10:30:59 +0800

    Remove

--- Source/ReflectionTool/Private/HelperUMG/CustomSpinBox.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "HelperUMG/CustomSpinBox.h"
-
-void UCustomSpinBox::SetTextSize(int32 NewSize)
-{
-	Font.Size = NewSize;
-}
